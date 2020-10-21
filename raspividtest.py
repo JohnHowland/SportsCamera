6c8ba01eeb39831ef69df380a891c5b96bce8824
John Howland <>
2020-10-20 22:57:27 -0400

    added converting files into one mp4 file after program ends

--- raspividtest.py
+++ raspividtest.py
@@ -4,6 +4,7 @@ import threading
 import time
 import string
 import time
+import datetime 
 #import pygame
 
 RASPIVIDCMD = "raspivid"
@@ -65,10 +66,24 @@ def stop_vid():
     #threading.Thread.__init__()
     subprocess.Popen("kill -USR1 `pidof raspivid`", shell=True)
 
+def setupFileSystem():
+    print "This is where setting up the file path will go"
+        
+
 #test program
 if __name__ == '__main__':
 
+    setupFileSystem()
+
+    folder_name = "/pi/home/SportsCameraVideos/%s" % datetime.datetime.now()
+    mkdir(foldername)
+    list_file_path = folder_name+"/list.txt"
+    list_fp = open(list_pile_path, "w")
+
     vid_index = 0
+
+    list_line_out = "%d.h264\n" % vid_index
+    list_fp.write(list_line_out)
     fileName = "/home/pi/Documents/%d.h264" %vid_index
     vid_index += 1
     init_vid(fileName)
@@ -85,11 +100,12 @@ if __name__ == '__main__':
         if x == "exit":
             break
         elif x == "capture":
+            list_line_out = "%d.h264\n" % vid_index
+            list_fp.write(list_line_out)
             fileName = "/home/pi/Documents/%d.h264" %vid_index
             vid_index += 1
             stop_vid()
             time.sleep(1.0)
- #           break
             init_vid(fileName)
             start_vid()
         else:
@@ -98,4 +114,15 @@ if __name__ == '__main__':
 
     print "Stopping raspivid controller"
     stop_vid()
+    list_fp.close()
     print "Done"
+
+    print "Creating single file"
+
+    mp4_out_filepath = folder_name+"out.mp4"
+
+    ffmpeg_out = "ffmpeg -f concat -i %s -c copy %s" % list_file_path, mp4_out_filepath
+    print ffmpeg_out
+    
+    subprocess.Popen(ffmpeg_out, shell=True)
+
