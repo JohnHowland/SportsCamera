1a7fcc84a4166683b5a8444059670b8984a8de1f
John Howland <>
2020-12-03 22:43:45 -0500

    added LED contols

--- LED_control.py
+++ LED_control.py
@@ -6,12 +6,19 @@ if __name__ == '__main__':
     GPIO.setwarnings(False) # Ignore warning for now
     GPIO.setmode(GPIO.BOARD)
 
+    GPIO.setup(7,GPIO.OUT)
+    GPIO.setup(11,GPIO.OUT)
+    GPIO.setup(13,GPIO.OUT)
+
     while True:
-        GPIO.setup(7,GPIO.OUT)
         print("LED on")
         GPIO.output(7,GPIO.HIGH)
+        GPIO.output(11,GPIO.HIGH)
+        GPIO.output(13,GPIO.HIGH)
         time.sleep(2)
         print("LED off")
         GPIO.output(7,GPIO.LOW)
+        GPIO.output(11,GPIO.LOW)
+        GPIO.output(13,GPIO.LOW)
         time.sleep(2)
 
