e2380a74acc3883066f9fd965559a80ee971c0fe
John Howland <>
2020-12-03 19:46:24 -0500

    added more buttons

--- bt_button.py
+++ bt_button.py
@@ -2,6 +2,7 @@
 
 import sys
 import evdev
+import socket
 
 class shutterButton():
     def __init__(self):
@@ -52,6 +53,18 @@ def get_BT_device_list():
                     print(evdev.categorize(event))
 
 if __name__ == "__main__":
-    bt = shutterButton()
-    bt.scan_for_devices()
+    #bt = shutterButton()
+    #bt.scan_for_devices()
     #get_BT_device_list()
+
+    serverMACAddress = '00:1f:e1:dd:08:3d'
+    port = 3
+    #s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+    s = socket.socket(socket.AF_BLUETOOTH, socket.AF_PACKET, socket.)
+    s.connect((serverMACAddress,port))
+    while 1:
+        text = input()
+        if text == "quit":
+            break
+        s.send(bytes(text, 'UTF-8'))
+    s.close()
--- phyical_button.py
+++ phyical_button.py
@@ -1,14 +1,27 @@
 import string
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 
-
+class button:
+    
 
 if __name__ == '__main__':
 
     GPIO.setwarnings(False) # Ignore warning for now
     GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
     GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+    GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(32, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
     while True: # Run forever
         if GPIO.input(16) == GPIO.HIGH:
-            print("Button was pushed!")
+            print("Button 16 was pushed!")
+
+        if GPIO.input(18) == GPIO.HIGH:
+            print("Button 18 was pushed!")
+
+        if GPIO.input(22) == GPIO.HIGH:
+            print("Button 22 was pushed!")
+
+        if GPIO.input(32) == GPIO.HIGH:
+            print("Button 32 was pushed!")
