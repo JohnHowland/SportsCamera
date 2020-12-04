3b3c9b50c993f3e003ec51609b231e487f782e92
John Howland <>
2020-12-03 22:49:05 -0500

    added LED contols

--- LED_control.py
+++ LED_control.py
@@ -6,19 +6,43 @@ if __name__ == '__main__':
     GPIO.setwarnings(False) # Ignore warning for now
     GPIO.setmode(GPIO.BOARD)
 
+    button16 = button.button(16)
+    button18 = button.button(18)
+    button22 = button.button(22)
+    button32 = button.button(32)
+
     GPIO.setup(7,GPIO.OUT)
     GPIO.setup(11,GPIO.OUT)
     GPIO.setup(13,GPIO.OUT)
 
+    LED1 = 0
+    LED2 = 0
+    LED3 = 0
+
     while True:
-        print("LED on")
-        GPIO.output(7,GPIO.HIGH)
-        GPIO.output(11,GPIO.HIGH)
-        GPIO.output(13,GPIO.HIGH)
-        time.sleep(2)
-        print("LED off")
-        GPIO.output(7,GPIO.LOW)
-        GPIO.output(11,GPIO.LOW)
-        GPIO.output(13,GPIO.LOW)
-        time.sleep(2)
+        
+        if button16.buttonIn() == 1:
+            if LED1 == 0:
+                GPIO.output(7,GPIO.HIGH)
+                LED1 = 1
+            else:
+                GPIO.output(7,GPIO.LOW)
+                LED1 = 0
+
+        if button18.buttonIn() == 1:
+            if LED2 == 0:
+                GPIO.output(11,GPIO.HIGH)
+                LED2 = 1
+            else:
+                GPIO.output(11,GPIO.LOW)
+                LED2 = 0
+
+        if button22.buttonIn() == 1:
+            if LED3 == 0:
+                GPIO.output(13,GPIO.HIGH)
+                LED3 = 1
+            else:
+                GPIO.output(13,GPIO.LOW)
+                LED3 = 0
+
 
