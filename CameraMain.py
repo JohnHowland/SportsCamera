d3153e6ae631f742f04a89ff70256a960ed1c672
John Howland <john.howland02@gmail.com>
2021-06-02 23:12:19 -0400

    edit

--- CameraMain.py
+++ CameraMain.py
@@ -20,7 +20,9 @@ def setupLogging():
 def rename_and_copy_file(selectedFolder, selectedFile):
     new_file_name = str(datetime.datetime.now())
 
-    rc = subprocess.Popen(['/home/pi/dev/SportsCamera/copyToDrive.sh', selectedFolder, selectedFile, "nothing", new_file_name], shell=True)
+    print(f"selectedFolder: {selectedFolder}, selectedFile: {selectedFile}, new_file_name: {new_file_name}")
+
+    rc = subprocess.Popen(['/home/pi/dev/SportsCamera/copyToDrive.sh', selectedFolder, selectedFile, "nothing", new_file_name], stdout=subprocess.PIPE)
     pass
 
 if __name__ == '__main__':
