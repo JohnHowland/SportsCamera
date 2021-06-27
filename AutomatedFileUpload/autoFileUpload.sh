00cb13abbaac081568f3ac96eb389137761caa08
John Howland <john.howland02@gmail.com>
2021-06-26 22:48:11 -0400

    edit

--- AutomatedFileUpload/autoFileUpload.sh
+++ AutomatedFileUpload/autoFileUpload.sh
@@ -9,15 +9,27 @@ else
     return
 fi
 
+echo "cding now"
 cd /home/pi/SportsCameraVideos
 
+echo "getting folders in dir"
 folderArray=( $( ls . ) )
 
-for folder in "${FOLDERS[@]}"; do
-    echo "Fold that "
+echo "starting loop now"
+for folder in "${folderArray[@]}"; do
+
     fileToCopy=( $( ls $folder/*.mp4 ) )
-    echo "File found: $fileToCopy"
+    
+    if [ -n "$fileToCopy" ]; then
+        echo "File found: $fileToCopy"
+
+        google_drive_path="sportsCam:SportsCamVideos/$newfile"
+        new_filepath=$fileToCopy
+
+        echo "rclone copy $new_filepath $google_drive_path"
+
+        #rclone copy $new_filepath $google_drive_path
+
 done
 
 
-#rclone copy $new_filepath $google_drive_path
